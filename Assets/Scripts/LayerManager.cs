481980880f56ab8beb3a0fea38864cd7913c5957
Thomas-Desktop2017 <junichifujio@gmail.com>
2018-03-28 00:12:52 -0700

    Tested shader box with purely virtual environment. Slight artifacting from obtuse viewing angles may be from box corners being out of view of the screen?
    Need: Incorporate automatic tagging / layering with layer manager.
    Need: Start ignoring objects inside of the box (Collide with virtual objects on the other side of the box, avoid colliding with stuff inside of the box (think Hololens)).
    Need: Fixing issue with Tango phone client API.

---
+++ Assets/Scripts/LayerManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CloakingBox
+{
+    public enum CloakLayers
+    {
+        Box,
+        Visible,
+        Invisible
+    };
+
+    public class LayerManager : MonoBehaviour
+    {
+        public string CloakingBoxLayer = "CloakingBox";
+        public string VisibleLayer = "Visible";
+        public string InvisibleLayer = "Invisible";
+
+        public static string CloakingBoxLayer_s = "CloakingBox";
+        public static string VisibleLayer_s = "Visible";
+        public static string InvisibleLayer_s = "Invisible";
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (!CloakingBoxLayer.Equals(CloakingBoxLayer))
+            {
+                // add the layer
+
+                // set the static layer to be the correct one
+                CloakingBoxLayer_s = CloakingBoxLayer;
+            }
+        }
+
+        public static LayerMask GetLayerMask(CloakLayers layer)
+        {
+            LayerMask mask = LayerMask.NameToLayer("Default");
+            switch (layer)
+            {
+                case CloakLayers.Box:
+                    mask = LayerMask.NameToLayer(CloakingBoxLayer_s);
+                    break;
+                case CloakLayers.Visible:
+                    mask = LayerMask.NameToLayer(VisibleLayer_s);
+                    break;
+                case CloakLayers.Invisible:
+                    mask = LayerMask.NameToLayer(InvisibleLayer_s);
+                    break;
+            }
+            return mask;
+        }
+    }
+}
